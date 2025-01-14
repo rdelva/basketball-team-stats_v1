7874070a45db3341d45be2b71bed07ba7f070b71
rdelva <roseldelva@gmail.com>
2025-01-14 02:52:22 -0500

    set up for loop for players_copy

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def intro():
 
 def  display_team_stats():
     print("Team Stats")
-    print(constants.PLAYERS)
+    #print(constants.PLAYERS)
 
       
 
@@ -29,7 +29,22 @@ def clean_data():
     print('CLEAN DATA:')        
     players_copy = copy.deepcopy(constants.PLAYERS)
     teams_copy = copy.deepcopy(constants.TEAMS)
-    players_new =  {}
+    players_new =  {} #created players_new to store cleaned data
+
+    for player in players_copy:
+        # player['name'] = player.name
+        # player['guardians'] = player.guardians
+        if(player['experience'] == 'YES'):
+            player['experience'] = True
+        else:
+            player['experience'] = False
+       
+        player['height'] = int(player['height'].split(" ")[0])
+        # = int(player['height']) + "inches"
+
+    print(player)
+
+
 
 
 

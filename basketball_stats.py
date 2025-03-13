b28ea7fd52996a81b3b60cbef3d0c86872791ec1
rdelva <roseldelva@gmail.com>
2025-03-12 21:30:56 -0400

    added logic for clean_data()

--- basketball_stats.py
+++ basketball_stats.py
@@ -2,9 +2,31 @@ import copy
 import constants #contains 2 constant lists called TEAMS and PLAYERS
 
 
+#Reads the existing player data from the PLAYERS constats
+def clean_data():
+    players_copy = copy.deepcopy(constants.PLAYERS)
+    teams_copy = copy.deepcopy(constants.TEAMS)
+    cleaned_player_list = []
+
+
+    for player in players_copy:
+        player['name']
+        
+        guardian_split = player['guardians'].split("and")
+        player['guardians'] = guardian_split
+
+        if(player['experience'] == 'YES'):
+            player['experience'] = True
+        else:
+            player['experience'] = False
+
+        player['height'] = int(player['height'].split(" ")[0])
+
+        return cleaned_player_list(player)
 
 def main():
     print("Hello from inside main")
+    clean_data()
 
 
 if __name__ == "__main__":

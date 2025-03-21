d144f36932d2cbf25b5a341ee17161879cea0996
rdelva <roseldelva@gmail.com>
2025-03-20 23:18:52 -0400

    created logic for selecting team choices

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
 import basketball_stats
 
+# Intro page for the basketball app
+
+def intro():
+    print("BASKETBALL TEAM STATS TOOL \n")
+    print ("---- MENU----\n")
+    print("Here are your choices: \n 1) Display Team Stats \n 2) Quit")
+    print("\n\n")
+    choice = int(input("Enter an option > "))
+    
+    if choice == 1:                       
+        basketball_stats.display_team_choices()    
+        team_choice = int(input("Enter an option:"))        
+        basketball_stats.display_team_choices(team_choice)    
+    elif choice == '2':
+         print("Ending Program ... Goodbye")
+         exit()
+    else:
+        print("Ending Program ... Goodbye")
+        exit()
 
 
 
-print("Hello from app.py")
 
 
 
 
 
 
-
-
-
-
-
+intro()
 
 
 

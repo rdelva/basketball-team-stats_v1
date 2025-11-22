305ed506291ca65aa7bb19789cda33cf06ef709d
rdelva <roseldelva@gmail.com>
2025-11-21 20:11:35 -0500

    added import and cleaned up clean data ()

--- app.py
+++ app.py
@@ -1,1 +1,2 @@
 import basketball_stats
+
--- basketball_stats.py
+++ basketball_stats.py
@@ -1,7 +1,17 @@
+import copy
 import constants
+
+
+# importing the teams dictionary and players dictionary 
+
+def clean_data():
+    
+    teams = copy.deepcopy(constants.TEAMS)
+    print(teams)
 
 def main():
     print("Hi")
+    clean_data()
 
 if __name__ == '__main__':
     main()
